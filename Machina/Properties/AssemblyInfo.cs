fb50e300e2db9d67fc7db031c119ea2f2bf6e967
Christopher King <chris@coyfox.net>
2020-05-31 16:34:12 -0600

    Updated csproj to 2017 and set target to dotnet core 3

--- Machina/Properties/AssemblyInfo.cs
+++ Machina/Properties/AssemblyInfo.cs
@@ -18,19 +18,6 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-
-[assembly: AssemblyTitle("Machina")]
-[assembly: AssemblyDescription("General-purpose library for raw network packet reading and reassembly")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Machina")]
-[assembly: AssemblyCopyright("Copyright © 2019 Ravahn")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -40,18 +27,4 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly: Guid("2681c607-a075-4070-be13-34f0a395119e")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.1.0")]
-[assembly: AssemblyInformationalVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("2.1.1.0")]
 
